01341bc48f27d5f6c8128eb6a335946fbbbaeff5
dianaesteves <desteves@users.noreply.github.com>
2019-04-01 11:21:19 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+GITHUB_REPO=mms-bosh-release
 # run migration checks for mms
-curl -O https://raw.githubusercontent.com/desteves/mongodb-release/master/jobs/ops_manager/templates/pre-start.sh
+curl -O https://raw.githubusercontent.com/desteves/${GITHUB_REPO}/master/jobs/ops_manager/templates/pre-start.sh
 bash pre-start.sh
 
 # run mms
-wget https://raw.githubusercontent.com/desteves/mongodb-release/master/jobs/ops_manager/templates/ctl.erb
+wget https://raw.githubusercontent.com/desteves/${GITHUB_REPO}/master/jobs/ops_manager/templates/ctl.erb
 chmod +x ctl.erb
 bash ctl.erb start
 
@@ -13,7 +14,7 @@ bash ctl.erb start
 
 
 # create global owner user...TODO
-wget https://raw.githubusercontent.com/desteves/mongodb-release/master/jobs/global_owner/templates/run.sh
+wget https://raw.githubusercontent.com/desteves/${GITHUB_REPO}/master/jobs/global_owner/templates/run.sh
 sed -i "s/USER=.*/USER=root/g" run.sh
 sed -i "s/PASS=.*/PASS=rootroot12345^/g" run.sh
 sed -i "s/FQDN=.*/FQDN=localhost/g" run.sh
